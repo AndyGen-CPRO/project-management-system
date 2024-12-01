992978a6023ed1f46565bc625ff4c2a90c4386a0
AndyGen-CPRO <christianandy.generoso.dl@gmail.com>
2024-12-01 15:35:58 -0700

    tailwind test

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/components/Home/HomePage.js",
+    "./src/components/Auth/Register.js", 
+    "./src/components/Auth/Login.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
