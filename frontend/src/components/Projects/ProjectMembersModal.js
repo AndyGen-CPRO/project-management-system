45dd24fa05423fbc1def4928152206fb189eecfc
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-12-02 23:56:04 -0700

    wierd stuff happended but fixed
    added cs to project overview

--- frontend/src/components/Projects/ProjectMembersModal.js
+++ frontend/src/components/Projects/ProjectMembersModal.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const ProjectMembers = ({ closeModal, project, token }) => {
     const [projMembers, setProjMembers] = useState([]);
+    const [receiverEmail, setReceiverEmail] = useState("");
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
     const [message, setMessage] = useState("");
-    const navigate = useNavigate();
 
-    useState(() => {
-        if(project) {
+    useEffect(() => {
+        if(project._id) {
             fetchMembers();
         }
-    })
+    },[])
+
     const fetchMembers = async () => {
         try {
             const response = await axios.get(`http://localhost:5000/project/${project._id}/members`, {
@@ -26,17 +28,53 @@ const ProjectMembers = ({ closeModal, project, token }) => {
         }
     };
 
-    const handleInvite = async () => {
+    const handleInvite = async (e) => {
+        e.preventDefault(); 
         try {
-            const responsee = await axios.post(`http://localhost:5000/projects/`)
+            const responsee = await axios.post(`http://localhost:5000/projects/${project._id}/invite`, {
+                receiverEmail,
+                title: "Project Invitation",
+                body: `You have been invited to Project ${project.name}.`
+            },
+                 {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        } catch (error) {
+            setMessage("Error inviting user.")
         }
     }
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-
+                <form onSubmit={handleInvite}>
+                    <label>Invite a user with email</label>
+                    <input
+                        type="text"
+                        onChange={(e) => setReceiverEmail(e.target.value)}
+                        required
+                    />
+                    <button type="submit">Send</button>
+                </form>
+                <div>
+                    {projMembers && (
+                        <>
+                            <h3>Current Members</h3>
+                            <ul>
+                            {projMembers.map(member => (
+                                <li key={member._id}>{member.userId.displayName}
+                                 - Joined At {member.JoinedAt} - {member.role}</li>
+                            ))}
+                            </ul>
+                        </>
+                    )}
+                </div>
+                <button onClick={closeModal}>Close</button>
             </div>
         </div>
     )
-}
+};
+
+export default ProjectMembers;
--- frontend/src/components/Projects/ProjectOverviewPage.js
+++ frontend/src/components/Projects/ProjectOverviewPage.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getToken } from '../../utils/auth';
 import ProjectDetails from './ProjectDetailsModal';
+import ProjectMembers from './ProjectMembersModal';
 
 const ProjectOverview = () => {
     const { id } = useParams();
@@ -10,6 +11,7 @@ const ProjectOverview = () => {
     const [message, setMessage] = useState("");
     const navigate = useNavigate();
     const [detailsModal, setDetailsModal] = useState(false);
+    const [membersModal, setMembersModal] = useState(false);
     const token = getToken();
 
     const fetchProject = async() => {
@@ -62,7 +64,13 @@ const ProjectOverview = () => {
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
                 <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">{project.name} Overview</h1>
-                <button onClick={() => {setDetailsModal(true)}} class="px-4 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" >Project Details</button>
+                <button onClick={() => {setDetailsModal(true)}} class="px-4 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Project Details</button>
+                <button onClick={() => {setMembersModal(true)}} class="px-4 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Project Members</button>
+                <button onClick={partsPage} class="px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Parts</button>
+                <button onClick={tasksPage} class="px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Tasks</button>
+                <div>
+                <button onClick={goBack} className="px-3 py-2 px-4 bg-gray-600 text-white font-semibold rounded-md shadow-md hover:bg-gray-700 transition duration-300">Back </button>
+                </div>
                 {detailsModal &&
                     <ProjectDetails 
                         closeModal={() => {setDetailsModal(false)}} 
@@ -70,12 +78,13 @@ const ProjectOverview = () => {
                         fetchProject={fetchProject} 
                         token={token}
                     />}
-                <button onClick={partsPage}class="px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Parts</button>
-                <button onClick={tasksPage}class="px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Tasks</button>
-                <div>
-                <button onClick={goBack}className="px-3 py-2 px-4 bg-gray-600 text-white font-semibold rounded-md shadow-md hover:bg-gray-700 transition duration-300">Back </button>
+                {membersModal &&
+                    <ProjectMembers 
+                        closeModal={() => {setMembersModal(false)}}
+                        project={project}
+                        token={token}
+                    />}
                 </div>
-            </div>
         </div>
     )
 }
