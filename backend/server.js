7515cfc5fc5789dceb9831d7147a487d01cd4009
AndyGen-CPRO <christianandy.generoso.dl@gmail.com>
2024-12-04 16:24:51 -0700

    Revert "Merge pull request #1 from AndyGen-CPRO/branch-Rob"
    This reverts commit c73eee73959da27c1bc6413b2f17336b7800506d, reversing
    changes made to ea1f3f4aecddb14bdfedc91550c78f648fe68b28.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectCreationRoutes")
 
 dotenv.config();
 connectDB();
@@ -15,9 +14,7 @@ app.use(express.json({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-//api routing 
-app.use("/api", authRoutes,projectRoutes);
-
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
-    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post("http://localhost:5000/api/login", { email, password });
             setMessage(response.data.message);
-            navigate('/ProjectOverview')
-            setMessage("Login is success")
         } catch (error) {
             setMessage("Log in Failed. Please try again.")
         }
